b69fd0530aae3a9fcaa8281bdd366a6b004e344a
etienne <siven_7@hotmail.com>
2020-04-21 11:56:52 +0200

    Working but not perfect :)

--- Dimension.cs
+++ Dimension.cs
@@ -2,7 +2,7 @@ namespace TickspeedChallenge
 {
     class Dimension
     {
-        static int[] _startCosts = { 2, 3, 5, 7, 10, 14, 18, 24 };
+        static int[] _startCosts = { 2, 3, 5, 13, 10, 14, 19, 25 };
         static int[] _stepCost = { 3, 4, 5, 6, 8, 10, 12, 15 };
 
         public int DimNumber { get; private set; }
@@ -10,20 +10,29 @@ namespace TickspeedChallenge
         public int StepCost { get; private set; }
         public int CurrentCost { get; private set; }
         public int StepsCount { get; private set; }
+        public int VisibleStepsCount { get; private set; }
+
+        public int NextCost
+        {
+            get { return CurrentCost + StepCost; }
+        }
 
         public Dimension(int dimNumber)
         {
             DimNumber = dimNumber;
-            StartCost =_startCosts[DimNumber-1];
-            StepCost = _stepCost[DimNumber-1];
+            StartCost = _startCosts[DimNumber - 1];
+            StepCost = _stepCost[DimNumber - 1];
             CurrentCost = StartCost;
             StepsCount = 0;
         }
 
-        public void Step()
+        public void Step(bool visible)
         {
-            CurrentCost+=StepCost;
+            CurrentCost += StepCost;
             StepsCount++;
+
+            if(visible)
+                VisibleStepsCount++;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,37 +6,77 @@ namespace TickspeedChallenge
 {
     class Program
     {
+        static int maxDimensions = 8;
         static List<Dimension> dimensions = new List<Dimension>();
-        
+
         static void Main(string[] args)
         {
-            for(int i=1; i <=8; i++)
+            for (int i = 1; i <= maxDimensions; i++)
                 dimensions.Add(new Dimension(i));
 
-            foreach(var d in dimensions) 
+            do
             {
-                d.Step();
-                Console.WriteLine(d.DimNumber + " " + d.StepsCount +  " " + d.CurrentCost);
-            }
+                var existingDims = GetExisitingDimensions().OrderBy(x => x.CurrentCost).ToList();
 
-            foreach(var d in GetExisitingDimensions()) 
-            {
-                d.Step();
-                Console.WriteLine(d.DimNumber + " " + d.StepsCount +  " " + d.CurrentCost);
-            }            
+                bool found=false;
+                foreach (var dim in existingDims)
+                {
+                    var otherDims = existingDims.ToList();
+                    otherDims.Remove(dim);
+                    
+                    if (!dimensions.Exists(x => x.CurrentCost == dim.NextCost) && 
+                        (
+                            !IsUnlocking(dim) ||
+                            (
+                                IsUnlocking(dim) &&
+                                !otherDims.Exists(x => x.CurrentCost == GetNextUnlocked(existingDims)?.CurrentCost)
+                            ) 
+                        )
+                    ) 
+                    {
+                        found=true;
+                        dim.Step(true);
+                        Console.WriteLine($"Buy dimension {dim.DimNumber} !");
+                        break;
+                    }
+                }
+
+                if(!found)
+                {
+                    var dim = existingDims.OrderBy(x => x.CurrentCost).First();
+                    dimensions.Where(x => x.CurrentCost == dim.NextCost).ToList().ForEach(x => x.Step(false));
+                    dim.Step(true);
+                    Console.WriteLine($"Buy dimension {dim.DimNumber} !");
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        static bool IsUnlocking(Dimension dim) {
+            var maxDim = GetExisitingDimensions().Max(x => x.DimNumber);
+            return  maxDim == dim.DimNumber && maxDim < maxDimensions;
+        }
+
+        static Dimension GetNextUnlocked(IEnumerable<Dimension> dims)
+        {
+            if (dimensions.Single(x => x.DimNumber == maxDimensions).StepsCount > 0)
+                return null;
+
+            var nextUnlocked = dimensions.SingleOrDefault(
+                x => x.DimNumber == dims.Max(x => x.DimNumber) + 1
+            );
+
+            return nextUnlocked;
         }
 
         static List<Dimension> GetExisitingDimensions()
         {
-            if(dimensions.Single(x => x.DimNumber==8).StepsCount > 0)
-                return dimensions;
-            
-            var existingDims = dimensions.Where(x => x.StepsCount > 0 || x.DimNumber == 1);
-            var nextUnlocked=dimensions.Single(
-                x => x.DimNumber == existingDims.Max(x => x.DimNumber)+1
-            );
-            existingDims.Append(nextUnlocked);
-
+            var existingDims = dimensions.Where(x => 
+                x.DimNumber == 1 ||
+                dimensions.Exists(y => 
+                    y.DimNumber == x.DimNumber - 1 && 
+                    y.VisibleStepsCount > 0
+                )
+            ).ToList();
             return existingDims.ToList();
         }
     }
