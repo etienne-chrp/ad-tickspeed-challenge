7f56464335ec509dcd0b1c3498427ef6b7c57322
etienne <siven_7@hotmail.com>
2020-04-21 08:49:34 +0200

    Init Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TickspeedChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
